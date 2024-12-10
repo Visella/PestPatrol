a962a6b5d299a8f4dd763760ff321f3fdacdd29b
Visella <136889489+Visella@users.noreply.github.com>
2024-12-10 23:19:49 +0700

    Update Detect.tsx

--- src/components/Detect.tsx
+++ src/components/Detect.tsx
@@ -33,7 +33,7 @@ const Detect: React.FC = () => {
 
     try {
       const response = await axios.post(
-        "https://api-pest-patrol.vercel.app:5000/predict",
+        "https://b980-103-84-6-70.ngrok-free.app/predict",
         formData,
         {
           headers: { "Content-Type": "multipart/form-data" },
